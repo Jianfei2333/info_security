8fcab3129c6d1aab69ee5ac41f50108a87c26150
Jianfei2333 <xjf999999@hotmail.com>
2018-05-24 19:15:41 +0800

    添加可视化界面

---
+++ DES_tool.py
@@ -0,0 +1,155 @@
+import DES
+import random
+
+# 2进制，16进制转换
+
+def _2hex(s):
+    return (hex(int(s,2))[2:])
+
+def _2bin(s):
+    b = bin(int(s,16))[2:]
+    delta = 0
+    if (len(b)%64 != 0):
+        delta = 64 - len(b)%64
+    b = "0"*delta + b
+    return (b)
+
+# 明文二进制编码，解码
+
+def encode(s):
+    return '100000'.join([bin(ord(c)).replace('0b', '') for c in s])
+
+def decode(s):
+    return ''.join([chr(i) for i in [int(b, 2) for b in s.split('100000')]])
+
+# IO处理
+
+def read_str(s):
+    str = input("请输入"+s+": ")
+    return (str)
+
+# 随机密钥生成
+
+def keygen():
+    # 生成随机密钥
+    # 长度：64bit
+    # 有效位数：56
+    # 其中奇偶校验分别为第8*n位
+    # 返回字符串，可以直接输入至加密、解密算法
+    flag = False
+    key = ""
+    for i in range(0,64):
+        if ((i+1)%8 == 0):
+            if (flag):
+                key = key + "1"
+            else:
+                key = key + "0"
+            continue
+        r = random.random()>0.5
+        # 每出现一个1，flag变化一次
+        # 七位里奇数次1时，flag为1
+        # 七位里偶数次1时，flag为0
+        if (r):
+            key = key + "1"
+            flag = not flag
+        else:
+            key = key + "0"
+    key_in_bin = bin(int(key, 2))[2:]
+    if (len(key_in_bin) != 64):
+        delta = 64 - len(key_in_bin)
+        key_in_bin = "0"*delta+key_in_bin
+    return {"bin": key_in_bin, "hex":_2hex(key_in_bin)}
+
+def cut(b, num):
+    # b = encode(s)
+    # 计算相差位数
+    delta = 0
+    if len(b)%num != 0:
+        delta = num - len(b)%num
+    # 补充差位（用0）
+    b = b+("0"*delta)
+    bins = []
+    while(len(b) != 0):
+        bins.append(b[:num])
+        b = b.split(b[:num],1)[1]
+    return {"data":bins, "addition_count":delta}
+
+def encryptor_cli():
+    # 明文：
+    plain = read_str("明文")
+    # 生成密钥：
+    key = keygen()
+    k = key["bin"]
+    print ("Your key: ", key["hex"])
+    # 编码:
+    b = encode(plain)
+    print ('encoding: ', b)
+    # 裁剪:
+    encrypt_package = cut(b, 64)
+    print ("Your additional bits count: ", encrypt_package["addition_count"])
+    cipher = ""
+    for d in encrypt_package["data"]:
+        node = DES.encrypt(d, k, silence=True)
+        cipher += node
+    print ('密文: '+_2hex(cipher))
+
+    print ('复制以下内容以解密：')
+    print (_2hex(cipher)+' '+str(encrypt_package["addition_count"])+' '+key["hex"])
+
+
+def decryptor_cli():
+    everything = read_str('密文，补充位数，密钥，用空格间隔')
+    cipher = everything.split(' ')[0]
+    addition = everything.split(' ')[1]
+    key = everything.split(' ')[2]
+    # 密文 cipher
+    # 补充位数 addition
+    # 密钥 key
+    k = _2bin(key)
+    # 裁剪：
+    decrypt_package = cut(_2bin(cipher), 64)
+    plain = ""
+    if (decrypt_package['addition_count']!=0):
+        print ("Error: Bits check failed, maybe the cipher you've input was wrong?")
+        return
+    else:
+        for d in decrypt_package["data"]:
+            node = DES.decrypt(d, k, silence=True)
+            plain += node
+    plain = plain[:-int(addition)]
+    print (plain)
+    print (decode(plain))
+
+# encryptor()
+# decryptor()
+
+def encryptor(plain, key):
+    # 编码:
+    k = _2bin(key)
+    b = encode(plain)
+    # 裁剪:
+    encrypt_package = cut(b, 64)
+    # 加密:
+    cipher = ""
+    for d in encrypt_package["data"]:
+        node = DES.encrypt(d, k, silence=True)
+        cipher += node
+
+    return {"cipher": _2hex(cipher), "addition": str(encrypt_package["addition_count"])}
+
+def decryptor(cipher, addition, key):
+    # 编码：
+    k = _2bin(key)
+    # 裁剪：
+    decrypt_package = cut(_2bin(cipher), 64)
+    plain = ""
+    if (decrypt_package['addition_count']!=0):
+        return {"err": "Error: Bits check failed, maybe the cipher you've input was wrong?"}
+    else:
+        for d in decrypt_package["data"]:
+            node = DES.decrypt(d, k, silence=True)
+            plain += node
+    # 去尾部
+    plain = plain[:-int(addition)]
+    
+    return ({"plain": decode(plain), "err": None})
---
+++ index.py
@@ -0,0 +1,77 @@
+import sys
+from PyQt4.QtCore import * 
+from PyQt4.QtGui import *
+import DES_ui
+import DES_tool
+ 
+# qtCreatorFile = "./UI/DES.ui" # Enter file here.
+ 
+# Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+ 
+Ui_MainWindow = DES_ui.Ui_MainWindow
+
+class MyApp(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.Key_gen_btn.clicked.connect(self.Key_gen_btn_clicked)
+        self.Encrypt_btn.clicked.connect(self.Encrypt_btn_clicked)
+        self.Key_copy_btn.clicked.connect(self.Key_copy_btn_clicked)
+        self.Decrypt_btn.clicked.connect(self.Decrypt_btn_clicked)
+
+    def warning(self, content):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Warning)
+        msg.setText(content)
+        # msg.setInformativeText("This is additional information")
+        # msg.setWindowTitle("Warning")
+        # msg.setDetailedText("The details are as follows:")
+        retval = msg.exec_()        
+        
+    def Encrypt_btn_clicked(self):
+        print ('you clicked Encrypt button')
+        plain = self.Plain_text.toPlainText()
+        plain = plain.replace(' ', '')
+        self.Plain_text.setPlainText(plain)
+        key = self.Key_text.toPlainText()
+        if (plain == '' or key == ''):
+            self.warning('Key is empty !')
+        else:
+            cipher_pack = DES_tool.encryptor(plain, key)
+            self.Cipher_text.setPlainText(cipher_pack["cipher"])
+            self.Addition_text.setPlainText(cipher_pack["addition"])
+            self.Plain_text2.setPlainText('')
+    
+    def Key_gen_btn_clicked(self):
+        newkey = DES_tool.keygen()
+        self.Key_text.setPlainText(newkey['hex'])
+        self.Cipher_text.setPlainText('')
+        self.Addition_text.setPlainText('')
+        self.Key_text2.setPlainText('')
+        self.Plain_text2.setPlainText('')
+
+    def Key_copy_btn_clicked(self):
+        self.Key_text2.setPlainText(self.Key_text.toPlainText())
+        self.Plain_text2.setPlainText('')
+
+    def Decrypt_btn_clicked(self):
+        print ('you clicked Decrypt button')
+        cipher = self.Cipher_text.toPlainText()
+        addition = self.Addition_text.toPlainText()
+        key = self.Key_text2.toPlainText()
+        if (key == ''):
+            self.warning('Key is empty !')
+        elif (cipher == ''):
+            self.warning('Cipher is empty !')
+        elif (addition == ''):
+            self.addition('Addition is empty !')
+        else:
+            plain = DES_tool.decryptor(cipher, addition, key)
+            self.Plain_text2.setPlainText(plain["plain"])
+ 
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
